92a10f5091c781bd2269ba9b106bca0f2bd3921c
LucianoRMaia <lucianomaia01@gmail.com>
2023-02-06 23:39:00 -0300

    Prototype working

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -15,17 +15,16 @@ class MediapipePose:
   def getPoseParams(self,image):
     image.flags.writeable = False
     results = self.pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    x,y,vis = [],[],[]
 
     if results.pose_landmarks != None:
       for point in results.pose_landmarks.landmark:
         #TODO HERE I HAVE TO SAVE EACH VALUE OF X Y AND VIS, CURRENTLY I'M JUST PASSING THE LAST VALUE, I'LL FIX TOMORROW
-        x = int(point.x*image.shape[1])
-        y = int(point.y*image.shape[0])
-        vis = point.visibility
-        cv2.circle(image, (x, y), radius=5, color=(225, 0, 100), thickness=-1)
-      return x,y,vis,image
-    else:
-      return [],[],[],image
+        x.append(int(point.x*image.shape[1]))
+        y.append(int(point.y*image.shape[0]))
+        vis.append(point.visibility)
+        cv2.circle(image, (x[-1], y[-1]), radius=5, color=(225, 0, 100), thickness=-1)
+    return x,y,vis,image
 
   def getOriginalPoseParams(self,originalImage):
     #TODO: modify this function to save the params and in the future it'll search the saved params instead of reprocessing the Original image again
@@ -37,13 +36,27 @@ class MediapipePose:
       print("We couldn't find any pose in the original image.")
 
 def drawPoses(image,xOrg,yOrg,visOrg,xCurrent,yCurrent,visCurrent):
-  for i in xOrg.len:
+  for i in range(len(xOrg)):
     #Draw original points
-    cv2.circle(image, (xOrg[i], yOrg[i]), radius=5, color=(0, 255, 0), thickness=1)
-    #draw current points
-    cv2.circle(image, (xCurrent[i], yCurrent[i]), radius=3, color=(255, 0, 0), thickness=1)
+    cv2.circle(image, (xOrg[i], yOrg[i]), radius=5, color=(255, 0, 0), thickness=1)
 
-  return image
+  currentMatchesOriginal = False
+  if len(xCurrent)>0:
+    currentMatchesOriginal = True
+    for i in range(len(xCurrent)):
+      if visCurrent[i]<0.5:
+        break
+      #draw current points
+      if (i==23 or i==24):
+        break
+      distanceBetweenPoints = np.sqrt((xOrg[i]-xCurrent[i])**2+(yOrg[i]-yCurrent[i])**2)
+      if distanceBetweenPoints <= 10: #current point is within acceptable range of original point
+        cv2.circle(image, (xCurrent[i], yCurrent[i]), radius=3, color=(0, 255, 0), thickness=-1)
+      else:
+        currentMatchesOriginal = False
+        cv2.circle(image, (xCurrent[i], yCurrent[i]), radius=3, color=(0, 0, 255), thickness=-1)
+
+  return image,currentMatchesOriginal
 
 
 def main():
@@ -52,7 +65,7 @@ def main():
 
   #Get params from the original image
   originalImage = cv2.imread("OriginalImage.jpg")
-  xOrg,yOrg,visOrg,image = mediapipePose.getOriginalPoseParams(originalImage)
+  xOrg,yOrg,visOrg,annotatedOriginalImage = mediapipePose.getOriginalPoseParams(originalImage)
 
   #Initializes the camera
   cap = cv2.VideoCapture(0)
@@ -63,13 +76,14 @@ def main():
   #Loops through the different sequential frames
   while cap.isOpened():
     success, imageCurrent = cap.read()
+    imageCurrentSave = imageCurrent.copy()
     if not success:
       print("Ignoring empty camera frame.")
       continue
 
     xCurrent,yCurrent,visCurrent,annotatedImage = mediapipePose.getPoseParams(imageCurrent)
 
-    resultImage = drawPoses(imageCurrent,xOrg,yOrg,visOrg,xCurrent,yCurrent,visCurrent)
+    resultImage,currentMatchesOriginal = drawPoses(imageCurrent,xOrg,yOrg,visOrg,xCurrent,yCurrent,visCurrent)
 
     # Draw the pose annotation on the image.
     #image.flags.writeable = True
@@ -80,8 +94,13 @@ def main():
     #    mp_pose.POSE_CONNECTIONS,
     #    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose', cv2.flip(resultImage, 1))
+    cv2.imshow('Resultado img Current', cv2.flip(annotatedImage, 1))
+    cv2.imshow('Resultado img Orginal', cv2.flip(annotatedOriginalImage, 1))
+    cv2.imshow('Resultado Completo', cv2.flip(resultImage, 1))
     if cv2.waitKey(1) & 0xFF == 27:
+      break
+    if currentMatchesOriginal:
+      cv2.imwrite("teste.jpg",imageCurrentSave)
       break
   cap.release()
 
